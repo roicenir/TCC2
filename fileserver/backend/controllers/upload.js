eac31313568f2794ea05359017745969f6ca4178
Ederson Torresini <ederson@boidacarapreta.cc>
2020-12-16 10:42:23 -0300

    Corrigir adição decabeçalhos

--- fileserver/backend/controllers/upload.js
+++ fileserver/backend/controllers/upload.js
@@ -1,11 +1,11 @@
 var fs = require("fs");
 
-module.exports = function(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+module.exports = function (req, res) {
+  // res.setHeader("Access-Control-Allow-Origin", "*");
   var arquivo = req.files.file;
   var temporario = req.files.file.path;
   var novo = "./uploads/" + req.files.file.name;
-  fs.rename(temporario, novo, function(err) {
+  fs.rename(temporario, novo, function (err) {
     if (err) {
       res.status(500).json({ error: err });
     }
--- fileserver/backend/server.js
+++ fileserver/backend/server.js
@@ -2,18 +2,17 @@ var express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
   multiparty = require("connect-multiparty");
+var cors = require("cors");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
 var router = express.Router();
 
-app.use("/api", router);
+app.use("/api", cors(), router);
 /*insira as rotas aqui */
 router.route("/upload").post(multiparty(), require("./controllers/upload"));
 
 app.listen(port);
 
 console.log("conectado a porta " + port);
-
-
